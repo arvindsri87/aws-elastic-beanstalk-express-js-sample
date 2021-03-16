d4c3de363faf42f86513a17a749b03317d9eba32
danwill70 <dan3770@gmx.de>
2021-03-16 11:38:22 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('My first tutorial'));
+app.get('/', (req, res) => res.send('Created pipeline with review'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
