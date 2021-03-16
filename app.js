b0e14293d6da6d944f958a00f13de49ff2ed829d
arvindsri87 <arvindenriq@gmail.com>
2021-03-16 12:25:16 +0100

    added new message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Created pipeline with review'));
+app.get('/', (req, res) => res.send('Simple Notification Service'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
